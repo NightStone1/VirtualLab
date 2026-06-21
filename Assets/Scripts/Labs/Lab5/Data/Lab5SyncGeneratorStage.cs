b60081178096cf6e9d1e7e7b52a053155ea07b9e
Terin Icheva <terina.babicheva.02@mail.ru>
2026-06-21 16:47:56 +0300

    modified lab5
    modified lab5

---
+++ Assets/Scripts/Labs/Lab5/Data/Lab5SyncGeneratorStage.cs
@@ -0,0 +1,14 @@
+public enum Lab5SyncGeneratorStage
+{
+    Intro,
+    PowerOn,
+    PrimeMoverStart,
+    NoLoadTest,
+    InductiveLoadTest,
+    ExternalTest,
+    RegulatingTest,
+    ShortCircuitTest,
+    ReactiveTriangle,
+    Completed,
+    Fault
+}
